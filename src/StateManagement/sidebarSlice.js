3d4c89caae23ae90de9d934101f0555962baba27
checkiamsiam <issiam02415@gmail.com>
2022-10-13 09:31:00 +0600

    task complete

--- src/StateManagement/sidebarSlice.js
+++ src/StateManagement/sidebarSlice.js
@@ -3,17 +3,35 @@ import { createSlice } from "@reduxjs/toolkit";
 const sidebarSlice = createSlice({
   name: "dashboard_sidebar",
   initialState: {
-    visible : false , 
+    visible: false,
+    animation: {
+      initial: {},
+      animate: {},
+      exit: {},
+      transition: {},
+    },
   },
   reducers: {
-    setVisible: (state , action) => {
+    setVisible: (state, action) => {
       state.visible = action.payload;
+    },
+    setInitialAnimation: (state, action) => {
+      state.animation.initial = action.payload;
+    },
+    setAnimateAnimation: (state, action) => {
+      state.animation.animate = action.payload;
+    },
+    setExitAnimation: (state, action) => {
+      state.animation.exit = action.payload;
+    },
+    setTransitionAnimation: (state, action) => {
+      state.animation.transition = action.payload;
     },
   },
 });
 
-const sidebarReducer = sidebarSlice.reducer
+const sidebarReducer = sidebarSlice.reducer;
 
-export const { setVisible } = sidebarSlice.actions;
+export const { setVisible, setInitialAnimation, setAnimateAnimation, setExitAnimation, setTransitionAnimation } = sidebarSlice.actions;
 
 export default sidebarReducer;
--- src/pages/Dashboard/Dashboard.index.js
+++ src/pages/Dashboard/Dashboard.index.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { motion, AnimatePresence } from "framer-motion";
-import { setVisible } from "../../StateManagement/sidebarSlice";
+import { setAnimateAnimation, setExitAnimation, setInitialAnimation, setTransitionAnimation, setVisible } from "../../StateManagement/sidebarSlice";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
   const { user, sidebar } = useSelector((state) => state);
+  useEffect(() => {
+    dispatch(setInitialAnimation({ x: 900, opacity: 0 }));
+    dispatch(setTransitionAnimation({ duration: 1, ease: "easeInOut" }));
+    dispatch(setAnimateAnimation({ x: 0, opacity: 1 }));
+    dispatch(setExitAnimation({ x: 900, opacity: 0 }));
+  }, [dispatch]);
   return (
     <div className="w-full h-screen flex justify-center items-center relative">
       <button onClick={() => dispatch(setVisible(true))} className="btn btn-primary">
@@ -14,10 +20,10 @@ const Dashboard = () => {
       <AnimatePresence>
         {sidebar.visible && (
           <motion.div
-            initial={{ x: 900, opacity: 0 }}
-            transition={{ duration: 1 , ease: "easeInOut" }}
-            animate={{ x: 0, opacity: 1 }}
-            exit={{ x: 900, opacity: 0 }}
+            initial={sidebar.animation.initial}
+            transition={sidebar.animation.transition}
+            animate={sidebar.animation.animate}
+            exit={sidebar.animation.exit}
             className={`absolute md:w-2/3 w-1/2 h-full bg-secondary right-0 `}
           >
             <div className="flex justify-end pt-5 pr-5">
