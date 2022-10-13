e11e3dfd1480a869bad3cdae9b44d9ff3211902b
checkiamsiam <issiam02415@gmail.com>
2022-10-13 09:51:45 +0600

    bug fix

--- src/components/RequireAuth/RequireAuth.js
+++ src/components/RequireAuth/RequireAuth.js
@@ -1,12 +1,14 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const RequireAuth = ({ children }) => {
   const {data} = useSelector((state) => state.user)
   let location = useLocation();
-  // const [user] = useAuthState(auth);
-  if (!data) {
+  const [user] = useAuthState(auth);
+  if (!user || !data) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
   return children;
